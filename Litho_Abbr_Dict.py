e7c240b3bbdad7bac9e93ac60f69b74a2c3a770c
mero2online <mero2online@gmail.com>
2022-04-20 20:05:25 +0200

    ADD: Copy Paste Cut SelectAll functions

--- Litho_Abbr_Dict.py
+++ Litho_Abbr_Dict.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import scrolledtext
+import pyperclip
 
 from Func import convertText
 from HelperFunc import resource_path
@@ -15,6 +16,48 @@ except:
 DOL: Wh, off wh, gy, sft, mod-hd, i/p.
 DOLOMITE: White, off white, grey, soft, moderately hard, in part.
 """
+
+
+def selectAllTxtBoxInput(*event):
+    txtBoxInput.tag_add(SEL, "1.0", END)
+    txtBoxInput.mark_set(INSERT, "1.0")
+    txtBoxInput.see(INSERT)
+
+    return "break"
+
+
+def selectAllTxtBoxOutput(*event):
+    txtBoxOutput.tag_add(SEL, "1.0", END)
+    txtBoxOutput.mark_set(INSERT, "1.0")
+    txtBoxOutput.see(INSERT)
+
+    return "break"
+
+
+def paste(*event):
+    text = txtBoxInput.selection_get(selection='CLIPBOARD')
+    txtBoxInput.insert('insert', text)
+
+    return "break"
+
+
+def copy(*event):
+    try:
+        text = txtBoxInput.selection_get()
+        text = txtBoxOutput.selection_get()
+        pyperclip.copy(text)
+    except TclError:
+        pass
+    return "break"
+
+
+def cut(*event):
+    txtBoxInput.clipboard_clear()
+    text = txtBoxInput.get("sel.first", "sel.last")
+    txtBoxInput.clipboard_append(text)
+    txtBoxInput.delete("sel.first", "sel.last")
+
+    return "break"
 
 
 def convertFromAbbr():
@@ -74,6 +117,34 @@ madeWithLoveBy = Label(
     root, text='Made with ❤ by Mohamed Omar', background='orange', foreground='#000000',
     font=('monospace', 9, 'bold'))
 madeWithLoveBy.place(x=490, y=595, width=190, height=20)
+
+mInput = Menu(root, tearoff=0)
+mInput.add_command(label="Select All", command=selectAllTxtBoxInput)
+mInput.add_command(label="Copy", command=copy)
+mInput.add_command(label="Cut", command=cut)
+mInput.add_command(label="Paste", command=paste)
+
+mOutput = Menu(root, tearoff=0)
+mOutput.add_command(label="Select All", command=selectAllTxtBoxOutput)
+mOutput.add_command(label="Copy", command=copy)
+
+
+def do_popup_input(event):
+    try:
+        mInput.tk_popup(event.x_root, event.y_root)
+    finally:
+        mInput.grab_release()
+
+
+def do_popup_output(event):
+    try:
+        mOutput.tk_popup(event.x_root, event.y_root)
+    finally:
+        mOutput.grab_release()
+
+
+txtBoxInput.bind("<Button-3>", do_popup_input)
+txtBoxOutput.bind("<Button-3>", do_popup_output)
 
 root.title('Litho_Abbr_Dict')
 root.geometry('700x615')
